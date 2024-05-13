47ec6d688a8d16a047bbddfedb77704356703c9a
jasurumarov <jasurumarov199@gmail.com>
2024-05-13 20:01:00 +0500

    finish

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -3,12 +3,18 @@ import { useSelector } from 'react-redux'
 
 // Components
 import CartContent from '../../components/cartContent/CartContent'
+import Empty from '../../components/empty/Empty'
 
 const Cart = () => {
   const carts = useSelector(s => s.cart.value)
   return (
     <div className='cart-page'>
+      {carts.length 
+        ?
       <CartContent data={carts}/>
+        :
+      <Empty/>
+      }
     </div>
   )
 }
